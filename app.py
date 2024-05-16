1689d1972e0421912b4d14ac773e5ba5d1c52025
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2024-05-17 00:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   # Log the request (replace with your preferred logging library/implementation)
+  print("Called")
   return 'Hello, World!'
 
 if __name__ == '__main__':
-  app.run(debug=False)  # Set debug to False for deployment
+  app.run(debug=True)  # Set debug to False for deployment
