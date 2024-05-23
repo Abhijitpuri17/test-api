8c95cc47e237b8b1de7030f78e53d441426da5ca
Abhijit Puri <85777554+Abhijitpuri17@users.noreply.github.com>
2024-05-23 12:51:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/headers', methods=['GET'])  # Only allow GET requests
+@app.route('/headers', methods=['POST'])  # Only allow GET requests
 def print_headers():
   """Prints all request headers"""
   headers = request.headers
+  body = request.get_json()
+  print(f"Request body: {body}")
   # Loop through headers dictionary and print key-value pairs
   for key, value in headers.items():
     print(f"{key}: {value}")
